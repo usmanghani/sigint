3551a5633f1e497824d24915d01f2918a5e379bd
devin <devin1891@gmail.com>
2016-08-28 19:37:03 -0700

    got segmenting to work

--- linear_analysis.py
+++ linear_analysis.py
@@ -1,15 +1,22 @@
 import sys
 import numpy as np
+
+from IPython import embed
+from math import ceil
 from pydub import AudioSegment
 
+
+import speech_recognition as sr
+
 def transcribe_audio_file(filename):
-    import speech_recognition as sr
     from os import path
     AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), filename)
 
     r = sr.Recognizer()
     with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source) # read the entire audio file
+        segments = [r.record(source, duration=10) for _ in range(int(ceil(source.DURATION/10.)))]
+
+    embed()
 
     # recognize speech using Google Speech Recognition
     try:
@@ -23,25 +30,29 @@ def transcribe_audio_file(filename):
         print "Could not request results from Google Speech Recognition service; {0}".format(e)
 
 
+def segment_audio(audio, dumb=True):
 
+    if dumb:
+        pass
+
+    # from pyAudioAnalysis import audioBasicIO as aIO
+    # from pyAudioAnalysis import audioSegmentation as aS
+    # # [Fs, x] = aIO.readAudioFile(audio_file)
+    # [Fs, x] = aIO.readAudioFile('temp.wav')
+    # segments = aS.silenceRemoval(x, Fs, 0.020, 0.020, smoothWindow = 1.0, Weight = 0.3, plot = False)
 
 audio_file = sys.argv[-1]
+#
+# audio = AudioSegment.from_mp3(audio_file)
+#
+# AUDIO_FILE = 'abcd.wav'
+# with sr.AudioFile(AUDIO_FILE) as source:
+#     audio = r.record(source) # read the entire audio file
 
-segment = AudioSegment.from_mp3(audio_file)
-
+transcribe_audio_file(audio_file)
+# embed()
 
 # seg1 = segment[241*1000:250*1000]
 # # from pydub.playback import play
 # # play(seg1)
 # seg1.export(out_f='temp.wav', format='wav', codec='flac')
-
-
-
-# print transcribe_audio_file('temp.wav')
-
-
-from pyAudioAnalysis import audioBasicIO as aIO
-from pyAudioAnalysis import audioSegmentation as aS
-# [Fs, x] = aIO.readAudioFile(audio_file)
-[Fs, x] = aIO.readAudioFile('temp.wav')
-segments = aS.silenceRemoval(x, Fs, 0.020, 0.020, smoothWindow = 1.0, Weight = 0.3, plot = False)
